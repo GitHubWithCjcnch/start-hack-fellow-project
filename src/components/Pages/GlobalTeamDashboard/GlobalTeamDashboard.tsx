a28a3d9d0264782f2d80dcf5d9c932e1421fe97d
caio vitor <githubwithcjcnch@gmail.com>
2024-03-21 12:53:35 -0300

    adding the main component to the page of globalteam

--- src/components/Pages/GlobalTeamDashboard/GlobalTeamDashboard.tsx
+++ src/components/Pages/GlobalTeamDashboard/GlobalTeamDashboard.tsx
@@ -1,11 +1,17 @@
 import { FC } from "react";
 import NavMenu from "./NavMenu/NavMenu";
+import Main from "./Main/Main";
 
 const GlobalTeamDashboard: FC = () => {
     return (
-        <>
-            <NavMenu />
-        </>
+        <div className="flex">
+          <div className="w-64 bg-gray-900 text-white min-h-screen">
+              <NavMenu />
+          </div>
+          <div className="flex-grow bg-gray-100 min-h-screen">
+              <Main />
+          </div>
+      </div>
     )
 }
 
---
+++ src/components/Pages/GlobalTeamDashboard/Main/Main.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Main: FC = (props) => {
+    return (
+        <div className="flex-grow min-h-screen overflow-auto">
+            AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA 
+        </div>
+    )
+}
+
+export default Main;
