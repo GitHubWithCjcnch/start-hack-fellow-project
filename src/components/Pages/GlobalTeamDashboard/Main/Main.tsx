9fa1e0e3fefe9a9842b5105ca67eece22fffc809
caio vitor <githubwithcjcnch@gmail.com>
2024-03-21 13:55:01 -0300

    corrcting the main page

--- src/components/Pages/GlobalTeamDashboard/Main/Main.tsx
+++ src/components/Pages/GlobalTeamDashboard/Main/Main.tsx
@@ -1,13 +1,29 @@
 import { FC } from "react";
+import Dashboard from "../Pages/Dashboard/Dashboard";
+import Startups from "../Pages/Startups/Startups";
+import Mentors from "../Pages/Mentors/Mentors";
+import Request from "../Pages/Request/Request";
 
 interface MainProps {
     page: any;
 }
 
 const Main: FC<MainProps> = ({ page }) => {
+    let Element: any;
+
+    if (page === "dashboard") {
+        Element = () => <Dashboard />
+    } else if (page === "startups") {
+        Element = () => <Startups />
+    } else if (page === "mentors") {
+        Element = () => <Mentors />
+    } else if (page === "request") {
+        Element = () => <Request />
+    }
+    
     return (
         <div className="flex-grow break-all overflow-auto">
-            {page}
+            <Element />
         </div>
     )
 }
