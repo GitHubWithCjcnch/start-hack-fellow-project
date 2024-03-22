11a834e2d2543d94ffe02ee6bf48212eff789b05
maxbrng <90261666+maxbrng@users.noreply.github.com>
2024-03-22 03:32:04 +0100

    fix styling Dashboard

--- src/components/Pages/GlobalTeamDashboard/Pages/Dashboard/Dashboard.tsx
+++ src/components/Pages/GlobalTeamDashboard/Pages/Dashboard/Dashboard.tsx
@@ -1,50 +1,91 @@
 import Notifications from "@/components/Reusable/Notifications";
-import SlackLogo from '../../../../../assets/slack_logo.svg'
-import NotionLogo from '../../../../../assets/notion_logo.png'
-import AsanaLogo from '../../../../../assets/asana_logo.png'
+import SlackLogo from "../../../../../assets/slack_logo.svg";
+import NotionLogo from "../../../../../assets/notion_logo.png";
+import AsanaLogo from "../../../../../assets/asana_logo.png";
 import { Link } from "react-router-dom";
-import Tasks  from "@/components/Reusable/Tasks/Tasks";
+import Tasks from "@/components/Reusable/Tasks/Tasks";
 import BadgeProgressTracker from "@/components/Reusable/BadgeProgressTracker";
 
 export default function Dashboard() {
-    return (
-        <div className="flex flex-col items-center justify-center">
-            <div className="h-screen space-y-10 ">
-                <div className="w-full flex justify-center">
-                    <span className="text-white text-4xl font-bold uppercase my-10">HI Paul!</span>
-                </div>
-                <div className="flex space-x-20">
-                    <div className="flex flex-col space-y-10">
-                        <div className="flex space-x-4">
-                            <Link className="flex flex-col items-center px-14 py-5 bg-[#333333] rounded transition-colors ease-in-out duration-200 hover:bg-[#444444]" to="">
-                                <img src={SlackLogo} alt="slack_logo" className="w-16 h-16 mb-2 object-contain" />
-                                <span className="font-bold text-white whitespace-nowrap">Slack</span>
-                            </Link>
-                            <Link className="flex flex-col items-center px-14 py-5 bg-[#333333] rounded transition-colors ease-in-out duration-200 hover:bg-[#444444]" to="">
-                                <img src={NotionLogo} alt="notion_logo" className="w-16 h-16 mb-2 object-contain" />
-                                <span className="font-bold text-white whitespace-nowrap">Notion</span>
-                            </Link>
-                            <Link className="flex flex-col items-center px-14 py-5 bg-[#333333] rounded transition-colors ease-in-out duration-200 hover:bg-[#444444]" to="">
-                                <img src={AsanaLogo} alt="asana_logo" className="w-16 h-16 mb-2 object-contain" />
-                                <span className="font-bold text-white whitespace-nowrap">Asana</span>
-                            </Link>
-                        </div>
-                        <Tasks />
-                    </div>
-                    <div className="w-2/4">
-                        <Notifications
-                            notification={{
-                                type: 'travel',
-                                title: 'Travel Request',
-                                company: 'Some Company Name'
-                            }}
-                        />
-                    </div>
-                </div>
-                <div>
-                    <BadgeProgressTracker />
-                </div>
-            </div>
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="w-[87.5%] pt-10 h-screen space-y-8 ">
+        <div className="w-full flex justify-center">
+          <span className="text-white text-5xl font-bold uppercase my-6">
+            HI Lorenz!
+          </span>
         </div>
-    )
+        <div className="flex flex-row gap-10">
+          <div className="flex flex-col space-y-8 w-full">
+            <div className="flex space-x-5">
+              <Link
+                className="flex flex-col items-center px-14 py-5 bg-[#333333] rounded-lg transition-colors ease-in-out duration-200 hover:bg-[#444444]"
+                to=""
+              >
+                <img
+                  src={SlackLogo}
+                  alt="slack_logo"
+                  className="w-16 h-16 mb-2 object-contain"
+                />
+                <span className="font-bold text-white whitespace-nowrap">
+                  Slack
+                </span>
+              </Link>
+              <Link
+                className="flex flex-col items-center px-14 py-5 bg-[#333333] rounded-lg transition-colors ease-in-out duration-200 hover:bg-[#444444]"
+                to=""
+              >
+                <img
+                  src={NotionLogo}
+                  alt="notion_logo"
+                  className="w-16 h-16 mb-2 object-contain"
+                />
+                <span className="font-bold text-white whitespace-nowrap">
+                  Notion
+                </span>
+              </Link>
+              <Link
+                className="flex flex-col items-center px-14 py-5 bg-[#333333] rounded-lg transition-colors ease-in-out duration-200 hover:bg-[#444444]"
+                to=""
+              >
+                <img
+                  src={AsanaLogo}
+                  alt="asana_logo"
+                  className="w-16 h-16 mb-2 object-contain"
+                />
+                <span className="font-bold text-white whitespace-nowrap">
+                  Asana
+                </span>
+              </Link>
+            </div>
+            <Tasks />
+          </div>
+          <div className="flex w-full">
+            <Notifications
+              notifications={[
+                {
+                  type: "travel",
+                  title: "Travel Request",
+                  company: "Paul Sofero (ArtisanCraft)",
+                },
+                {
+                  type: "coaching",
+                  title: "Coaching Session",
+                  company: "John Stone (EduTech Solutions)",
+                },
+                {
+                  type: "intro",
+                  title: "Introduction",
+                  company: "Frank Collins (Fintech Solutions)",
+                },
+              ]}
+            />
+          </div>
+        </div>
+        <div>
+          <BadgeProgressTracker />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Reusable/Notifications.tsx
+++ src/components/Reusable/Notifications.tsx
@@ -1,43 +1,66 @@
-import { FC } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlane, faClipboard, faMoneyBillWave, faClock, faUserFriends, faBell } from '@fortawesome/free-solid-svg-icons';
+import { FC } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlane,
+  faClipboard,
+  faMoneyBillWave,
+  faClock,
+  faUserFriends,
+  faBell,
+} from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
+interface Notification {
+  type: "travel" | "coaching" | "reimburse" | "attendance" | "intro";
+  title: string;
+  company: string;
+}
 
 interface MainProps {
-    notification: {
-        type: 'travel' | 'coaching' | 'reimburse' | 'attendance' | 'intro',
-        title: string,
-        company: string
-    };
+  notifications: Notification[];
 }
 
 const ICONS = {
-    'travel': <FontAwesomeIcon icon={faPlane} />,
-    'coaching': <FontAwesomeIcon icon={faClipboard} />,
-    'reimburse': <FontAwesomeIcon icon={faMoneyBillWave} />,
-    'attendance': <FontAwesomeIcon icon={faClock} />,
-    'intro': <FontAwesomeIcon icon={faUserFriends} />,
+  travel: <FontAwesomeIcon icon={faPlane} size="sm" />,
+  coaching: <FontAwesomeIcon icon={faClipboard} size="sm" />,
+  reimburse: <FontAwesomeIcon icon={faMoneyBillWave} size="sm" />,
+  attendance: <FontAwesomeIcon icon={faClock} size="sm" />,
+  intro: <FontAwesomeIcon icon={faUserFriends} size="xs" />,
 };
 
-const Notifications: FC<MainProps> = ({ notification }) => {
-    const { type, title, company } = notification;
-
-    return (
-        <div className="mx-auto max-w-lg min-h-full p-5 rounded-sm bg-[#191919] text-white">
-            <div className="flex items-center mb-5">
-                <FontAwesomeIcon icon={faBell} />
-                <h2 className="ml-2">Notifications</h2>
+const Notifications: FC<MainProps> = ({ notifications }) => {
+  return (
+    <div className="w-full p-5 rounded-lg bg-[#191919] text-white gap-3.5 flex flex-col">
+      <div className="font-bold text-lg flex items-center space-x-3">
+        <FontAwesomeIcon icon={faBell} />
+        <h2 className="ml-2">Notifications</h2>
+      </div>
+      {notifications.map((item, index) => (
+        <div className="flex flex-row p-4 rounded-md bg-[#333333] gap-2.5">
+          <div className="flex items-center justify-center bg-[#191919] rounded-full h-8 w-8">
+            {ICONS[item.type]}
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="flex items-center">
+              <h4 className="font-bold">{item.title}</h4>
             </div>
-            <div className="p-4 rounded-md bg-[#333333]">
-                <div className="flex items-center mb-4">
-                    <div className="bg-[#191919] rounded-full px-2 py-1">
-                        {ICONS[type]}
-                    </div>
-                    <h4 className="ml-2 font-bold">{title}</h4>
-                </div>
-                <div>from <span className='bg-[#460004] rounded-sm p-1.5'>{company}</span></div>
+            <div className="flex items-center gap-1.5 text-sm">
+              <p>from</p>
+              <span className="bg-[#460004] rounded-sm p-1.5 font-medium text-sm">
+                {item.company}
+              </span>
             </div>
+          </div>
         </div>
-    );
-}
+      ))}
+      <Link
+        to="/dashboards#dashboard"
+        className="font-bold text-[#FE8902] hover:text-[#ff8f42]"
+      >
+        View all Meetings
+      </Link>
+    </div>
+  );
+};
 
 export default Notifications;
